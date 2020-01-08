8c6d1b5810ae25f81557b63eddad306b30eacdf6
國展 <zhan16061@gmail.com>
2020-01-08 17:56:16 +0800

    add env

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var linebot = require('linebot')
-var config = require('./config')
 
 var bot = linebot({
-  channelId: config.CHANNEL_ID,
-  channelSecret: config.CHANNEL_SECRET,
-  channelAccessToken: config.CHANNEL_ACCESS_TOKEN
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
 bot.on('message', function (event) {
